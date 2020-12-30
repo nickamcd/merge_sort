f22dee794753d71fa157855d271d11e8ec6f8fb7
Nick McDaniel <nickamcd@gmail.com>
2020-12-30 12:12:38 -0800

    Merge added and refactored with rubocop.

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,20 +1,32 @@
+# frozen_string_literal: true
+
 def merge_sort(arr)
-  if arr.length < 2
+  # Base case
+  if arr.length <= 1
     arr
   else
-    # Sort left half of elements.
-    left = merge_sort(arr[0..arr.length/2])
-    # Sort right half of elements.
-    right = merge_sort(arr[arr.length/2..array.length])
-    # Merge sorted halves.
+    # Sort left side of array.
+    left = merge_sort(arr.slice(0...arr.length / 2))
+    # Sort right side of array.
+    right = merge_sort(arr.slice(arr.length / 2..-1))
+    # Merge both sides
     merge(left, right)
   end
 end
 
-def merge(left, right)
-  sorted = []
-
-  left.each do 
-    left[0] < right[0] ? sorted << left.shift : sorted << right.shift
+def merge(left, right, sorted=[])
+  # Merge until both arrays are empty.
+  unless left.empty? && right.empty?
+    # Compare for the lesser value.
+    sorted << if left[0] < right[0]
+                left.shift
+              else
+                right.shift
+              end
   end
+  # Combine all arrays and return with lesser values in front.
+  sorted + left + right
 end
+
+
+p merge_sort([42, 742, 223, 19, 6, 1111, 482, 99])
